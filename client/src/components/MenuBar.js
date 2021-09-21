209d44d617a39d534be14dee0ddc0bcfb2b95e5e
rudiruhl <rudigd82@gmail.com>
2021-09-21 17:09:07 +0200

    :sparkles: Authentication Context.

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
+
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext)
   const pathname = window.location.pathname;
 
   const path = pathname === '/' ? 'home' : pathname.substr(1);
@@ -10,9 +14,23 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
-      
-        <Menu pointing secondary size="massive" color="blue">
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="blue">
+          <Menu.Item
+            name={user.username}
+            active
+            as={Link}
+            to="/"
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+                name='logout'
+                onClick={logout}
+            />
+          </Menu.Menu>
+        </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="blue">
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
@@ -38,8 +56,9 @@ function MenuBar() {
             />
           </Menu.Menu>
         </Menu>
-    )
-  
+  )
+
+  return menuBar;
 }
 
 export default MenuBar;
