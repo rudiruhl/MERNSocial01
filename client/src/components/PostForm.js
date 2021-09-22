47b8af06f4094dcc031f7eb43e4326fdaac63d63
rudiruhl <rudigd82@gmail.com>
2021-09-22 18:37:41 +0200

    :art: Improve error catching.

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -33,22 +33,32 @@ function PostForm() {
     }
     
     return (
-        <Form onSubmit={onSubmit}>
-            <h2>Create a new post:</h2>
+        <>
+          <Form onSubmit={onSubmit}>
+            <h2>Create a post:</h2>
             <Form.Field>
-                <Form.Input
-                    placeholder="Moin."
-                    name="body"
-                    onChange={onChange}
-                    value={values.body}
-                />
-                <Button type="submit" color="red">
-                    Submit
-                </Button>
-            </Form.Field>            
-        </Form>
-    )
-}
+              <Form.Input
+                placeholder="Hi World!"
+                name="body"
+                onChange={onChange}
+                value={values.body}
+                error={error ? true : false}
+              />
+              <Button type="submit" color="teal">
+                Submit
+              </Button>
+            </Form.Field>
+          </Form>
+          {error && (
+            <div className="ui error message" style={{ marginBottom: 20 }}>
+              <ul className="list">
+                <li>{error.graphQLErrors[0].message}</li>
+              </ul>
+            </div>
+          )}
+        </>
+      );
+    }
 
 const CREATE_POST_MUTATION = gql`
 mutation createPost($body: String!){
