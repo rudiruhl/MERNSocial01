0e2308361d85b471a834ad2d0281cecc6077d237
rudiruhl <rudigd82@gmail.com>
2021-09-22 11:44:41 +0200

    :sparkles: Adding posts functionality.

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,15 +4,25 @@ import { ApolloClient } from '@apollo/client';
 import { InMemoryCache } from '@apollo/client';
 import { createHttpLink } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:5000'
-})
+});
+
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken');
+    return{
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    };
+});
 
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
-})
+});
 
 export default (
     <ApolloProvider client={client}>
---
+++ client/src/components/PostForm.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Button, Form } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+import { useForm } from '../util/hooks';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+function PostForm() {
+
+    const { values, onChange, onSubmit } = useForm(createPostCallback, {
+        body: ''
+    });
+
+    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+        variables: values,
+        update(proxy, result){
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            });
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data: {
+                getPosts: [result.data.createPost, ...data.getPosts],
+                },
+            });
+            values.body= '';
+        },
+    });
+
+    function createPostCallback(){
+        createPost();
+    }
+    
+    return (
+        <Form onSubmit={onSubmit}>
+            <h2>Create a new post:</h2>
+            <Form.Field>
+                <Form.Input
+                    placeholder="Moin."
+                    name="body"
+                    onChange={onChange}
+                    value={values.body}
+                />
+                <Button type="submit" color="red">
+                    Submit
+                </Button>
+            </Form.Field>            
+        </Form>
+    )
+}
+
+const CREATE_POST_MUTATION = gql`
+mutation createPost($body: String!){
+    createPost(body: $body){
+        id body createdAt username
+        likes{
+            id username createdAt
+        }
+        likeCount
+        comments{
+            id body username createdAt
+        }
+        commentCount
+    }
+}
+`
+
+
+export default PostForm;
